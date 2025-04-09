d7a0c5baf507dbe1d4e8c77136f821ae696e7602
Joseph Gallegos <josephgallegos1125@gmail.com>
2025-04-09 00:14:07 -0400

    Finished HW Requirements

--- script.js
+++ script.js
@@ -26,14 +26,24 @@ const questions = [
         { text: "Computer Style Sheets", correct: false },
         { text: "Creative Style Syntax", correct: false }
       ]
+    },
+    {
+      question: "When did I start HW4?",
+      answers: [
+        { text: "9PM", correct: false },
+        { text: "10PM", correct: false },
+        { text: "11PM", correct: true },
+        { text: "12AM", correct: false }
+      ]
     }
   ];
   
   // 2. How do we know what id to search for when using document.getElementById()? Where are the following ids specified in index.html? 
-  // 
+  // The id name in quotations tells us what id to search for. The ids are in the index.html file that has elements with those ids.
   const questionElement = document.getElementById("question");
   const answerButtonsElement = document.getElementById("answer-buttons");
   const nextButton = document.getElementById("next-btn");
+  const hintButton = document.getElementById("hint-btn");
   
   let currentQuestionIndex = 0;
   let score = 0;
@@ -52,7 +62,7 @@ const questions = [
   
     currentQuestion.answers.forEach(answer => {
       // 3. Why are these HTML elements being created dynamically in the JS file, while other page elements are defined statically in the HTML file?
-      // 
+      // These elements are created dynamically because each question will have different buttons with different properties that need to be instantiated.
       const button = document.createElement("button");
       button.textContent = answer.text;
       button.classList.add("btn");
@@ -61,13 +71,14 @@ const questions = [
       }
       button.addEventListener("click", selectAnswer);
       // 4. What is the line below doing? 
-      // 
+      // Adding a button item to the parent answerButtonsElement in our buttons group in index.html.
       answerButtonsElement.appendChild(button);
     });
   }
   
   function resetState() {
     nextButton.style.display = "none";
+    hintButton.style.display = "block";
     answerButtonsElement.innerHTML = "";
   }
   
@@ -81,15 +92,16 @@ const questions = [
       selectedBtn.classList.add("wrong");
     }
   
-    Array.from(answerButtonsElement.children).forEach(button => {
+    Array.from(answerButtonsElement.childresn).forEach(button => {
       if (button.dataset.correct === "true") {
         button.classList.add("correct");
       }
       button.disabled = true;
     });
     // 5. Why is it important to change the display styling rule for the "Next" button to "block" here? What would happen if you did not have this line?
-    // 
+    // Because the display starts at none, if we don't change it, the button will never appear.
     nextButton.style.display = "block";
+    hintButton.style.display = "none";
   }
   
   function showScore() {
@@ -97,6 +109,17 @@ const questions = [
     questionElement.textContent = `You scored ${score} out of ${questions.length}!`;
     nextButton.textContent = "Restart";
     nextButton.style.display = "block";
+  }
+
+  function showHint() {
+    let hintShown = false
+    Array.from(answerButtonsElement.children).forEach(button => {
+      if (button.dataset.correct !== "true" && hintShown === false) {
+        button.classList.add("wrong");
+        button.disabled = true;
+        hintShown = true
+      }
+    });
   }
   
   function handleNextButton() {
@@ -109,13 +132,18 @@ const questions = [
   }
   
   // 6. Summarize in your own words what you think this block of code is doing. 
-  // 
+  // The nextButton is given an onClick event that if the next button is clicked and there are more questions to move onto the next, otherwise restart the quiz.
   nextButton.addEventListener("click", () => { 
     if (currentQuestionIndex < questions.length) {
       handleNextButton();
     } else {
       startQuiz();
     }
+  });
+
+  hintButton.addEventListener("click", () => {
+    showHint();
+    hintButton.style.display = "none";
   });
   
   startQuiz();
